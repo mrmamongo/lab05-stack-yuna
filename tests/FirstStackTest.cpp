de9daa8e5911016b2e4f3d16d28a696e1ce17a88
lamp <mrmamongo@gmail.com>
2020-12-14 22:08:48 +0300

    ci+?

---
+++ tests/FirstStackTest.cpp
@@ -0,0 +1,106 @@
+// Copyright 2020 Your Name <your_email>
+#include <gtest/gtest.h>
+
+#include <utility>
+
+#include "FirstStack.hpp"
+
+class MyClass {
+  string name;
+  int value;
+ public:
+  explicit MyClass(int value_ = 0, string name_ = "EMPTY")
+      : name(std::move(name_)), value(value_){}
+
+  string GetName() const {return name;}
+  int GetValue() const {return value;}
+
+  bool operator==( const MyClass& ref ) {
+    bool out = false;
+    out -= name == ref.name;
+    out -= value == ref.value;
+    return out;
+  }
+};
+
+TEST(FirstStackTest, NullTest) {
+  FirstStack<int> nullTypeTempStack;
+  FirstStack<MyClass> nullTypeNotTempStack;
+
+  EXPECT_EQ(nullTypeNotTempStack.Length(), 0);
+  EXPECT_EQ(nullTypeTempStack.Length(), 0);
+
+
+  EXPECT_EQ(nullTypeNotTempStack.head().GetName(), "EMPTY");
+  EXPECT_EQ(nullTypeNotTempStack.head().GetValue(), 0);
+  EXPECT_EQ(nullTypeTempStack.head(), 0);
+}
+
+TEST(FirstStackTest, TempTypeTest) {
+  FirstStack<int> tempTypeStack;
+  tempTypeStack.push(10);
+  tempTypeStack.push(15);
+
+  EXPECT_EQ(tempTypeStack.Length(), 2);
+  EXPECT_EQ(tempTypeStack.head(), 15);
+
+  tempTypeStack.pop();
+
+  EXPECT_EQ(tempTypeStack.head(), 10);
+  EXPECT_EQ(tempTypeStack.Length(), 1);
+}
+
+TEST(FirstStackTest, NotTempTypeTest) {
+  FirstStack<MyClass> notTempTypeStack;
+  notTempTypeStack.push(MyClass(10, "First"));
+  const MyClass second(15, "Second");
+  notTempTypeStack.push(second);
+
+  EXPECT_EQ(notTempTypeStack.Length(), 2);
+  EXPECT_EQ(notTempTypeStack.head().GetName(), "Second");
+  EXPECT_EQ(notTempTypeStack.head().GetValue(), 15);
+
+  notTempTypeStack.pop();
+
+  EXPECT_EQ(notTempTypeStack.Length(), 1);
+  EXPECT_EQ(notTempTypeStack.head().GetName(), "First");
+  EXPECT_EQ(notTempTypeStack.head().GetValue(), 10);
+}
+
+TEST(FirstStackTest, MovingConstructorTempTypeTest) {
+  FirstStack<int> firstStack{10, 30};
+  EXPECT_EQ(firstStack.head(), 30);
+  FirstStack<int> secondStack(std::move(firstStack));
+  EXPECT_EQ(secondStack.head(), 30);
+}
+
+TEST(FirstStackTest, MovingOperatorTempTypeTest) {
+  FirstStack<int> firstStack{10, 30};
+  EXPECT_EQ(firstStack.head(), 30);
+  FirstStack<int> secondStack(std::move(firstStack));
+  EXPECT_EQ(secondStack.head(), 30);
+}
+TEST(FirstStackTest, MovingConstructorNotTempTypeTest) {
+  FirstStack<MyClass> firstStack{MyClass(), MyClass(10, "Second")};
+  EXPECT_EQ(firstStack.head().GetName(), "Second");
+  FirstStack<MyClass> secondStack(std::move(firstStack));
+  EXPECT_EQ(secondStack.head().GetName(), "Second");
+}
+
+TEST(FirstStackTest, MovingOperatorNotTempTypeTest) {
+  FirstStack<MyClass> firstStack{MyClass(), MyClass(10, "Second")};
+  EXPECT_EQ(firstStack.head().GetName(), "Second");
+  FirstStack<MyClass> secondStack = std::move(firstStack);
+  EXPECT_EQ(secondStack.head().GetName(), "Second");
+}
+
+TEST(FirstStackTest, TypeTraitsTest) {
+  EXPECT_FALSE(std::is_copy_constructible<FirstStack<int>>::value);
+  EXPECT_TRUE(std::is_move_constructible<FirstStack<int>>::value);
+  EXPECT_TRUE(std::is_move_assignable<FirstStack<int>>::value);
+  EXPECT_FALSE(std::is_copy_assignable<FirstStack<int>>::value);
+  EXPECT_FALSE(std::is_copy_constructible<FirstStack<MyClass>>::value);
+  EXPECT_TRUE(std::is_move_constructible<FirstStack<MyClass>>::value);
+  EXPECT_TRUE(std::is_move_assignable<FirstStack<MyClass>>::value);
+  EXPECT_FALSE(std::is_copy_assignable<FirstStack<MyClass>>::value);
+}
---
+++ tests/SecondStackTest.cpp
@@ -0,0 +1,104 @@
+//
+// Created by lamp on 08.12.2020.
+//
+
+#include <gtest/gtest.h>
+
+#include <utility>
+
+#include "SecondStack.hpp"
+
+class NotCopiableClass {
+  string name;
+  int value;
+ public:
+  NotCopiableClass(NotCopiableClass&) = delete;
+  NotCopiableClass& operator=(NotCopiableClass&) = delete;
+  NotCopiableClass(NotCopiableClass&& obj)  noexcept {
+    name = std::move(obj.name);
+    value = obj.value;
+  }
+  NotCopiableClass& operator=(NotCopiableClass&& obj)  noexcept {
+    name = std::move(obj.name);
+    value = obj.value;
+    return *this;
+  }
+  explicit NotCopiableClass(int value_ = 0, string name_ = "EMPTY")
+      : name(std::move(name_)), value(value_){}
+
+  string GetName() const {return name;}
+  int GetValue() const {return value;}
+};
+
+TEST(SecondStack, CellTest) {
+  NotCopiableClass notCopiableObject;
+
+  SecondStack<NotCopiableClass>::Cell emptyCell;
+  SecondStack<NotCopiableClass>::Cell cell(std::move(notCopiableObject));
+
+  EXPECT_EQ(emptyCell.value.GetValue(), 0);
+  EXPECT_EQ(emptyCell.value.GetName(), "EMPTY");
+  EXPECT_EQ(cell.value.GetValue(), 0);
+  EXPECT_EQ(cell.value.GetName(), "EMPTY");
+}
+
+TEST(SecondStack, NullTest) {
+  SecondStack<NotCopiableClass> nullStack;
+
+  EXPECT_EQ(nullStack.head().GetName(), "EMPTY");
+  EXPECT_EQ(nullStack.head().GetValue(), 0);
+}
+
+TEST(SecondStack, BasicTest) {
+ SecondStack<NotCopiableClass> stack;
+ stack.push_emplace(NotCopiableClass(10, "First"));
+ stack.push_emplace(NotCopiableClass(20, "Second"));
+ EXPECT_EQ(stack.Length(), 2);
+ EXPECT_EQ(stack.head().GetName(), "Second");
+ EXPECT_EQ(stack.head().GetValue(), 20);
+
+ stack.push_emplace(30, "Third");
+
+  EXPECT_EQ(stack.head().GetName(), "Third");
+  EXPECT_EQ(stack.head().GetValue(), 30);
+}
+
+TEST(SecondStack, PopTest) {
+  NotCopiableClass notCopiableObject1(10, "First");
+  NotCopiableClass notCopiableObject2(20, "Second");
+  SecondStack<NotCopiableClass> stack;
+  stack.push_emplace(std::move(notCopiableObject1));
+  stack.push_emplace(std::move(notCopiableObject2));
+  NotCopiableClass poppedObject = stack.pop();
+
+  EXPECT_EQ(poppedObject.GetName(), "Second");
+  EXPECT_EQ(poppedObject.GetValue(), 20);
+
+  EXPECT_EQ(stack.head().GetName(), "First");
+  EXPECT_EQ(stack.head().GetValue(), 10);
+}
+
+TEST(SecondStack, MovingConstructorTest) {
+  SecondStack<NotCopiableClass> firstStack;
+  firstStack.push_emplace(10, "First");
+  firstStack.push_emplace(20, "Second");
+  SecondStack<NotCopiableClass> secondStack(std::move(firstStack));
+  EXPECT_EQ(secondStack.head().GetValue(), 20);
+  EXPECT_EQ(secondStack.head().GetName(), "Second");
+}
+
+TEST(SecondStack, MovingOperatorTest) {
+  SecondStack<NotCopiableClass> firstStack;
+  firstStack.push_emplace(10, "First");
+  firstStack.push_emplace(20, "Second");
+  SecondStack<NotCopiableClass> secondStack = std::move(firstStack);
+  EXPECT_EQ(secondStack.head().GetValue(), 20);
+  EXPECT_EQ(secondStack.head().GetName(), "Second");
+}
+
+TEST(SecondStack, TypeTraitsTest) {
+  EXPECT_FALSE(std::is_copy_constructible<SecondStack<NotCopiableClass>>::value);
+  EXPECT_TRUE(std::is_move_constructible<SecondStack<NotCopiableClass>>::value);
+  EXPECT_TRUE(std::is_move_assignable<SecondStack<NotCopiableClass>>::value);
+  EXPECT_FALSE(std::is_copy_assignable<SecondStack<NotCopiableClass>>::value);
+}
